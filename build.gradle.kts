eefa0ddc1f08b9d47b28fc53d659f3d08891aefc
Andrey Stoyan <andrey.stoyan.csam@gmail.com>
2022-02-18 14:47:34 +0300

    Setup tests and run

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.6.10"
+    application
 }
 
 group = "ru.itmo.sd.nebash"
@@ -11,4 +12,17 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    testImplementation(platform("org.junit:junit-bom:5.8.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.withType<JavaExec>().all {
+    mainClass.set("ru.itmo.sd.nebash.MainKt")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
 }
---
+++ src/test/kotlin/ru/itmo/sd/nebash/MainKtTest.kt
@@ -0,0 +1,11 @@
+package ru.itmo.sd.nebash
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class MainKtTest {
+    @Test
+    fun main() {
+        assertTrue(false)
+    }
+}
